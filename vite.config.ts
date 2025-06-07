0d6aedda8553df27d114b542517b535508ca1e20
SanJi_ThinkBook <wangsz86@163.com>
2025-06-07 15:29:50 +0800

    fix: 调整前端代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://114.55.73.26:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
   },
 })
